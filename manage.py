f43233e9f4516a210909602d3ee9ccb18be763b8
Bogdan Levkiv <levkiv.b@gmail.com>
2015-12-28 23:53:45 +0200

    os.system(ls)

--- manage.py
+++ manage.py
@@ -4,9 +4,10 @@ import os
 from lists import *
 
 if __name__ == "__main__":
+#    os.system("ls")
     print("main")
     path = "dir"
     files = local_list_files(path)
-    #files = os.listdir(path)
     for file in files:
         print(file)
+        os.system("cp " + file + " dir2/")
